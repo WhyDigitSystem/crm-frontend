fa508110f3f145399ae46cc77b357177ff3e90cc
DiveshWDS <divesh@whydigit.in>
2025-11-13 11:33:30 +0530

    toggle button for sidebar

--- src/layout/MainLayout/Header/index.js
+++ src/layout/MainLayout/Header/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Avatar, Box, Chip } from '@mui/material';
+import { Avatar, Box, Chip, ButtonBase } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 import LogoSection from '../LogoSection';
@@ -10,6 +10,7 @@ import GlobalSection from './GlobalSection';
 import MenuBar from './GlobalSection/MenuBar';
 import { showToast } from 'utils/toast-component';
 import apiCalls from 'apicall';
+import { IconMenu2 } from '@tabler/icons-react';
 
 const Header = ({ handleLeftDrawerToggle }) => {
   const [logo, setLogo] = useState(null);
@@ -54,11 +55,29 @@ const Header = ({ handleLeftDrawerToggle }) => {
       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 200, height: 65 }}>
         <LogoSection />
       </Box>
-
+      <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
+        <Avatar
+          variant="rounded"
+          sx={{
+            ...theme.typography.commonAvatar,
+            ...theme.typography.mediumAvatar,
+            transition: 'all .2s ease-in-out',
+            background: theme.palette.secondary.light,
+            color: theme.palette.secondary.dark,
+            '&:hover': {
+              background: theme.palette.secondary.dark,
+              color: theme.palette.secondary.light
+            }
+          }}
+          onClick={handleLeftDrawerToggle}
+          color="inherit"
+        >
+          <IconMenu2 stroke={1.5} size="1.3rem" />
+        </Avatar>
+      </ButtonBase>
       <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
         <SearchSection />
       </Box>
-
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
         <Avatar
           sx={{
